2d3237e09b77e1c5c0dbd1392d1c3d31448617a4
ddouglass <ddouglass@DDOUGLASS>
2022-08-05 15:18:00 -0400

    Add project files.

---
+++ DapperCRUD/Customer.cs
@@ -0,0 +1,11 @@
+﻿namespace DapperCRUD
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = String.Empty;
+        public string FirstName { get; set; } = String.Empty;
+        public string LastName { get; set; } = String.Empty;
+        public string Location { get; set; } = String.Empty;
+    }
+}
---
+++ DapperCRUD/CustomerController.cs
@@ -0,0 +1,67 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace DapperCRUD
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+        public CustomerController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpGet]
+
+        public async Task<ActionResult<List<Customer>>> GetAllCustomers()
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            IEnumerable<Customer> customer = await SelectAllCustomers(connection);
+            return Ok(customer);
+        }
+
+        [HttpGet("{customerId}")]
+
+        public async Task<ActionResult<List<Customer>>> GetCustomer(int customerId)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            var customer = await connection.QueryFirstAsync<Customer>("select * from customers where Id = @Id",
+                new { Id = customerId});
+            return Ok(customer);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<Customer>>> CreateCustomer(Customer customer)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            await connection.ExecuteAsync(
+                "insert into customers (name, firstname, lastname, location) values (@Name, @FirstName, @LastName, @Location)", customer);
+            return Ok(await SelectAllCustomers(connection));
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<Customer>>> UpdateCustomer(Customer customer)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            await connection.ExecuteAsync(
+                "update customers set name = @Name, firstname = @FirstName, lastname = @LastName, location = @Location where id = @Id", customer);
+            return Ok(await SelectAllCustomers(connection));
+        }
+
+        [HttpDelete("{customerId}")]
+        public async Task<ActionResult<List<Customer>>> DeleteCustomer(int customerId)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            await connection.ExecuteAsync("delete from customers where id = @Id", new { Id = customerId });
+            return Ok(await SelectAllCustomers(connection));
+        }
+        private static async Task<IEnumerable<Customer>> SelectAllCustomers(SqlConnection connection)
+        {
+            return await connection.QueryAsync<Customer>("select * from customers");
+        }
+    }
+}
